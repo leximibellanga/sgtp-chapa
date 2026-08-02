0eaccf2acdfed7d8586cf82bc3bd0795d24f60f5
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-02 21:22:26 +0200

    feat(frontend): Implementar carroApi.
    Implentar carroApi para consumir dados dos carros cadastrados na base de dados. Nesse aquivo contem todos os endpoints definidos no backend.

---
+++ sgtp-frontend/src/api/carroApi.js
@@ -0,0 +1,28 @@
+import axiosClient from "./axiosClient"
+
+
+export function listarCarros(apenasAtivos) {
+    const params = apenasAtivos !== undefined ? { apenasAtivos } : {}
+    return axiosClient.get("/carros", { params })
+}
+
+export function buscarCarroPeloId(id) {
+    return axiosClient.get(`/carros/${id}`)
+}
+
+export function criarCarro(dados) {
+    return axiosClient.post("/carros", dados);
+}
+
+export function atualizarCarro(id, dados) {
+    return axiosClient.put(`/carros/${id}`, dados)
+}
+
+export function desativarCarro(id) {
+    return axiosClient.patch(`/carros/${id}/desativar`)
+}
+
+export function ativarCarro(id) {
+    return axiosClient.patch(`/carros/${id}/ativar`)
+}
+
