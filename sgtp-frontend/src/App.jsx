06c10c7f6e7860c2964c853cd7c0a0cd81236325
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-06 14:25:37 +0200

    refactor(): Adicionar o arquivo ListaCarrosPage a sua rota.
    Closes #26

--- sgtp-frontend/src/App.jsx
+++ sgtp-frontend/src/App.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
+import { BrowserRouter, Navigate, replace, Route, Routes } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './routes/ProtectedRoute.route'
+import RoleRoute from './routes/RoleRoute.route'
+import LoginPage from "./pages/auth/LoginPage"
+import AdminLayout from './components/layout/AdminLayout'
+import MotoristaLayout from './components/layout/MotoristaLayout'
+import ListaCarrosPage from './pages/admin/carros/ListaCarrosPage'
+import { Toaster } from 'sonner'
 
 const App = () => {
     return (
-        <div>
-            <h1 className='text-2xl font-bold'>API chapa</h1>
-        </div>
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/login" element={<LoginPage />} />
+
+                    <Route element={<ProtectedRoute />}>
+                        <Route element={<RoleRoute roleExigida={"ADMIN"} />}>
+                            <Route element={<AdminLayout />}>
+                                <Route path="/admin/dashboard" element={<div>Dashboad (em construcao)</div>} />
+                                <Route path="/admin/carros" element={<ListaCarrosPage />} />
+                                <Route path="/admin/usuarios" element={<div>Usuarios (em construcao)</div>} />
+                                <Route path="/admin/registos" element={<div>Registos (em construcao)</div>} />
+                                <Route path="/admin/gastos" element={<div>Gastos (em construcao)</div>} />
+                                <Route path="/admin/relatorios" element={<div>Relatorios (em construcao)</div>} />
+                            </Route>
+                        </Route>
+
+                        <Route element={<RoleRoute roleExigida={"MOTORISTA"} />}>
+                            <Route element={<MotoristaLayout />}>
+                                <Route path="/motorista/dashboard" element={<div>Dashboad (em construcao)</div>} />
+                                <Route path="/motorista/registos" element={<div>Registos (em construcao)</div>} />
+                            </Route>
+                        </Route>
+                    </Route>
+
+                    <Route path="/" element={<Navigate to={"/login"} replace />} />
+                </Routes>
+            </AuthProvider>
+
+            {/* Notificacoes */}
+            <Toaster position='top-center' richColors/>
+        </BrowserRouter>
     )
 }
 
