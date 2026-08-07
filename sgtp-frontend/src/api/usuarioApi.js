068e897038e8f15cec00a6eb8126e4a26bc02ce1
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-07 19:56:03 +0200

    feat(frontend): Implementar usuarioApi
    Implentar usuarioApi para consumir dados dos usuarios cadastrados na base de dados. Nesse aquivo contem todos os endpoints definidos no backend para gerenciar os usuarios.

---
+++ sgtp-frontend/src/api/usuarioApi.js
@@ -0,0 +1,33 @@
+import axiosClient from "./axiosClient";
+
+// Listar usuarios - GET
+export function listarUsuarios(role) {
+  const params = role ? { role } : {};
+  return axiosClient.get("/usuarios", { params });
+}
+
+// Buscar usuario pelo ID - GET
+export function buscarUsuarioPeloId(id) {
+  return axiosClient.get(`/usuarios/${id}`);
+}
+
+// Criar novo usuario - POST
+export function criarUsuario(dados) {
+  return axiosClient.post("/usuarios", dados);
+}
+
+// Atualizar usuario pelo ID - PUT
+export function atualizarUsuarioPeloId(id, dados) {
+    return axiosClient.put(`/usuarios/${id}`, dados)
+}
+
+// Aativar usuario pelo ID - PATCH
+export function ativarUsuarioPeloId(id) {
+    return axiosClient.patch(`/usuarios/${id}/ativar`)
+}
+
+// Desativar usuario pelo ID - PATCH
+export function desativarUsuarioPeloId(id) {
+    return axiosClient.patch(`/usuarios/${id}/desativar`)
+}
+
