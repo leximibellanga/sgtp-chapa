45e948d24e9f52ba97c4352bdc1671409362d76b
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-10 18:45:11 +0200

    refactor(frontend): Organizar estilizacao.
    Organizar estilizacao de modo que a propriedade text esteja alinhada, ou seja, propriedades de texto fica uma ao lado da outra( size e color)

--- sgtp-frontend/src/components/ui/Input.jsx
+++ sgtp-frontend/src/components/ui/Input.jsx
@@ -19,7 +19,7 @@ const Input = forwardRef(
           <input
             ref={ref}
             type={tipoReal}
-            className={`w-full bg-emerald-950/5 border border-emerald-950/20 rounded-lg px-3.5 py-2.5 text-sm outline-none transition focus:ring-2 focus:ring-emerald-950/30 text-emerald-950/80 focus:border-verde-mata ${isSenha ? "pr-10" : ""}`}
+            className={`w-full bg-emerald-950/5 border border-emerald-950/20 rounded-lg px-3.5 py-2.5 text-sm text-emerald-950/80 outline-none transition focus:ring-2 focus:ring-emerald-950/30 focus:border-verde-mata ${isSenha ? "pr-10" : ""}`}
             {...props}
           />
 
