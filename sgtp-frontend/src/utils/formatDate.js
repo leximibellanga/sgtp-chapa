2b588549c0c76e53e1fa0dbd0cb78f10298ba90b
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-11 22:47:24 +0200

    refactor(frontend): adicionar dia da semana
    A nova formatacao nas datas contem la o dia da semana tambem: [seg, ter, qua, qui, sex, sab, dom]. Usar abreviaturas para a data nao ser muito extensa na tabela.
    A formatacao final eh: Qua, 12 Ago / 2026

--- sgtp-frontend/src/utils/formatDate.js
+++ sgtp-frontend/src/utils/formatDate.js
@@ -13,19 +13,25 @@ const meses = [
   "Dez",
 ];
 
-export default function formatarData(data) {
+const diaSemanaString = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+
+export default function formatarDate(data) {
   // pegar data completa: 12-08-2026T12:12:49 = 12/08/2026
   const criadoEm = new Date(data);
 
+  // formatar dia da semana: [dom, seg, ter, qua, qui, sex, sab]
+  const diaSemana = criadoEm.getDay();
   // formatar dia
   const dia =
-    criadoEm.getDate() > 10 ? criadoEm.getDate() : `0${criadoEm.getDate()}`;
+    criadoEm.getDate() >= 10 ? criadoEm.getDate() : `0${criadoEm.getDate()}`;
   // formatar mes
   const mes = meses[criadoEm.getMonth()];
   // formatar ano
   const ano = criadoEm.getFullYear();
 
   // Organizar e exibir a data completa
-  const dataFormatada = `${dia} / ${mes}. / ${ano}`;
+  const dataFormatada = `${diaSemanaString[diaSemana]}, ${dia} ${mes} / ${ano}`;
   return dataFormatada;
 }
+
+
