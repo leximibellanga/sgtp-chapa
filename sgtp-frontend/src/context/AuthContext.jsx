ad5af9a946bff9fc7a0637916f71163800e61808
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-26 23:28:05 +0200

    style(frontend): formatacao no arquivo de authContext

--- sgtp-frontend/src/context/AuthContext.jsx
+++ sgtp-frontend/src/context/AuthContext.jsx
@@ -1,43 +1,43 @@
 import { createContext, useEffect, useState } from "react";
-import { login as loginRequest } from "../api/authApi"
+import { login as loginRequest } from "../api/authApi";
 
-export const AuthContext = createContext(null)
+export const AuthContext = createContext(null);
 
 export function AuthProvider({ children }) {
-    const [usuario, setUsuario] = useState(null);
-    const [carregando, setCarregando] = useState(true)
+  const [usuario, setUsuario] = useState(null);
+  const [carregando, setCarregando] = useState(true);
 
-    useEffect(() => {
-        const usuarioSalvo = localStorage.getItem("usuario");
-        const token = localStorage.getItem("token")
+  useEffect(() => {
+    const usuarioSalvo = localStorage.getItem("usuario");
+    const token = localStorage.getItem("token");
 
-        if (usuarioSalvo && token) {
-            setUsuario(JSON.parse(usuarioSalvo));
-        }
-        setCarregando(false);
-    }, [])
-
-    async function login(email, senha) {
-        const resposta = await loginRequest(email, senha)
-        const { token, nome, email: emailResp, role } = resposta.data;
-
-        const dadosUsuario = { nome, email: emailResp, role };
-
-        localStorage.setItem("token", token);
-        localStorage.setItem("usuario", JSON.stringify(dadosUsuario));
-        setUsuario(dadosUsuario);
-
-        return dadosUsuario
+    if (usuarioSalvo && token) {
+      setUsuario(JSON.parse(usuarioSalvo));
     }
+    setCarregando(false);
+  }, []);
 
-    function logout() {
-        localStorage.removeItem("token");
-        localStorage.removeItem("usuario");
-    }
+  async function login(email, senha) {
+    const resposta = await loginRequest(email, senha);
+    const { token, nome, email: emailResp, role } = resposta.data;
 
-    return (
-        <AuthContext.Provider value={{usuario, login, logout, carregando}}>
-            {children}
-        </AuthContext.Provider>
-    )
+    const dadosUsuario = { nome, email: emailResp, role };
+
+    localStorage.setItem("token", token);
+    localStorage.setItem("usuario", JSON.stringify(dadosUsuario));
+    setUsuario(dadosUsuario);
+
+    return dadosUsuario;
+  }
+
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("usuario");
+  }
+
+  return (
+    <AuthContext.Provider value={{ usuario, login, logout, carregando }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
