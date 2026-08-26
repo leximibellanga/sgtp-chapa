3c2a3054a095b63e3987ba780ef88e0959523618
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-26 23:19:22 +0200

    style(frontend): formatacao e alguns comentarios no codigo no arquivo App

--- sgtp-frontend/src/App.jsx
+++ sgtp-frontend/src/App.jsx
@@ -1,58 +1,68 @@
-import React from 'react'
-import { BrowserRouter, Navigate, replace, Route, Routes } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
-import ProtectedRoute from './routes/ProtectedRoute.route'
-import RoleRoute from './routes/RoleRoute.route'
-import LoginPage from "./pages/auth/LoginPage"
-import AdminLayout from './components/layout/AdminLayout'
-import MotoristaLayout from './components/layout/MotoristaLayout'
-import ListaCarrosPage from './pages/admin/carros/ListaCarrosPage'
-import { Toaster } from 'sonner'
-import ListaUsuariosPage from './pages/admin/usuarios/ListaUsuariosPage'
-import ListaRegistosPage from './pages/admin/registos/ListaRegistosPage'
-import ListaRegistosMotoristaPage from './pages/motorista/ListaRegistosMotoristaPage'
-import FormRegistoPage from './pages/motorista/FormRegistoPage'
-import ListaGastosPage from './pages/admin/gastos/ListaGastosPage'
-import DashboardPage from './pages/admin/DashboardPage'
-import RelatoriosPage from './pages/admin/relatorios/RelatorioPage'
-import DashboardMotoristaPage from './pages/motorista/DashboardMotoristaPage'
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import ProtectedRoute from "./routes/ProtectedRoute.route";
+import RoleRoute from "./routes/RoleRoute.route";
+import LoginPage from "./pages/auth/LoginPage";
+import AdminLayout from "./components/layout/AdminLayout";
+import MotoristaLayout from "./components/layout/MotoristaLayout";
+import ListaCarrosPage from "./pages/admin/carros/ListaCarrosPage";
+import { Toaster } from "sonner";
+import ListaUsuariosPage from "./pages/admin/usuarios/ListaUsuariosPage";
+import ListaRegistosPage from "./pages/admin/registos/ListaRegistosPage";
+import ListaRegistosMotoristaPage from "./pages/motorista/ListaRegistosMotoristaPage";
+import FormRegistoPage from "./pages/motorista/FormRegistoPage";
+import ListaGastosPage from "./pages/admin/gastos/ListaGastosPage";
+import DashboardPage from "./pages/admin/DashboardPage";
+import RelatoriosPage from "./pages/admin/relatorios/RelatorioPage";
+import DashboardMotoristaPage from "./pages/motorista/DashboardMotoristaPage";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<LoginPage />} />
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
 
-                    <Route element={<ProtectedRoute />}>
-                        <Route element={<RoleRoute roleExigida={"ADMIN"} />}>
-                            <Route element={<AdminLayout />}>
-                                <Route path="/admin/dashboard" element={<DashboardPage />} />
-                                <Route path="/admin/carros" element={<ListaCarrosPage />} />
-                                <Route path="/admin/usuarios" element={<ListaUsuariosPage />} />
-                                <Route path="/admin/registos" element={<ListaRegistosPage />} />
-                                <Route path="/admin/gastos" element={<ListaGastosPage />} />
-                                <Route path="/admin/relatorios" element={<RelatoriosPage />} />
-                            </Route>
-                        </Route>
+          <Route element={<ProtectedRoute />}>
+            {/* ADMIN */}
+            <Route element={<RoleRoute roleExigida={"ADMIN"} />}>
+              <Route element={<AdminLayout />}>
+                <Route path="/admin/dashboard" element={<DashboardPage />} />
+                <Route path="/admin/carros" element={<ListaCarrosPage />} />
+                <Route path="/admin/usuarios" element={<ListaUsuariosPage />} />
+                <Route path="/admin/registos" element={<ListaRegistosPage />} />
+                <Route path="/admin/gastos" element={<ListaGastosPage />} />
+                <Route path="/admin/relatorios" element={<RelatoriosPage />} />
+              </Route>
+            </Route>
 
-                        <Route element={<RoleRoute roleExigida={"MOTORISTA"} />}>
-                            <Route element={<MotoristaLayout />}>
-                                <Route path="/motorista/dashboard" element={<DashboardMotoristaPage />} />
-                                <Route path="/motorista/registos" element={<ListaRegistosMotoristaPage />} />
-                                <Route path="/motorista/registos/novo" element={<FormRegistoPage />} />
-                            </Route>
-                        </Route>
-                    </Route>
+            {/* MOTORISTA */}
+            <Route element={<RoleRoute roleExigida={"MOTORISTA"} />}>
+              <Route element={<MotoristaLayout />}>
+                <Route
+                  path="/motorista/dashboard"
+                  element={<DashboardMotoristaPage />}
+                />
+                <Route
+                  path="/motorista/registos"
+                  element={<ListaRegistosMotoristaPage />}
+                />
+                <Route
+                  path="/motorista/registos/novo"
+                  element={<FormRegistoPage />}
+                />
+              </Route>
+            </Route>
+          </Route>
+          {/* Pagina inicial de Login */}
+          <Route path="/" element={<Navigate to={"/login"} replace />} />
+        </Routes>
+      </AuthProvider>
 
-                    <Route path="/" element={<Navigate to={"/login"} replace />} />
-                </Routes>
-            </AuthProvider>
+      {/* Notificacoes */}
+      <Toaster position="top-center" duration={3000} closeButton richColors />
+    </BrowserRouter>
+  );
+};
 
-            {/* Notificacoes */}
-            <Toaster position='top-center' duration={3000} closeButton richColors/>
-        </BrowserRouter>
-    )
-}
-
-export default App
+export default App;
