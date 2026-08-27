ae527e9fa2df8a7df0c8a11a4425ed43e472a84b
Leximíbel Langa <langaleximibel@gmail.com>
2026-08-27 18:20:53 +0200

    refactor(backend): consumir valor da variavel de ambiente
    Eliminar o valor estatico da url do frontend e passar a consumir  a url do frontend atraves de uma variavel de ambiente.

--- sgtp-backend/src/main/java/com/leximibel/sgtp_backend/config/CorsConfig.java
+++ sgtp-backend/src/main/java/com/leximibel/sgtp_backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.leximibel.sgtp_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         WebMvcConfigurer.super.addCorsMappings(registry);
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // url do front
+                .allowedOrigins(frontendUrl) // url do front
                 .allowedMethods("GET", "POST", "PUT", "PUT", "PATCH", "DELETE", "OPTIONS") // Http Methods
                 .allowedHeaders("*")
                 .allowCredentials(true);
